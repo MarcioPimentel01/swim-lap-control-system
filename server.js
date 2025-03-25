508bafad5cc39342e179984d2197bac8693668ab
Marcio Pimentel <149727150+MarcioPimentel01@users.noreply.github.com>
2025-03-25 16:55:37 -0400

    My expresser is serving my HTML file.

--- server.js
+++ server.js
@@ -5,3 +5,10 @@ const app = express(); //!This creates an instance of an Express app, kind of li
 console.log("hey, here is my web server", typeof app)
 console.log("is .listen a function?", typeof app.listen === "function")
 console.log("is .get a function?", typeof app.get === "function")
+
+app.use(express.static("public"));
+
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000/dashboard.html");
+});
