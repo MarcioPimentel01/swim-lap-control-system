e46ff17109dbaf93fc515bcfbc05b63993018025
Marcio Pimentel <149727150+MarcioPimentel01@users.noreply.github.com>
2025-03-26 16:30:39 -0400

    advent listner added with logic for values iqual or less than zero

--- public/dashboard.js
+++ public/dashboard.js
@@ -5,8 +5,8 @@ const socket = io(); //! 👈 connects to the server
 const poolSizeInput = document.getElementById("pool-size");
 console.log("pool size input", poolSizeInput);
 
-const start = document.querySelector(".start");
-console.log("Start Button", start);
+const startButton = document.querySelector(".start-button");
+console.log("Start Button", startButton);
 
 const lapDisplay = document.querySelector(".lap-display");
 console.log("lap Display", lapDisplay);
@@ -16,4 +16,21 @@ console.log("Lap counter", lapCounter);
 
 const lapTime = document.querySelector(".lap-time");
 console.log("Display lap time", lapTime);
+
+
+// ! wire up the Start Session button to detect when it’s clicked and grab the pool size input.
+
+startButton.addEventListener("click", () => {
+    const poolSize = Number(poolSizeInput.value);
+    console.log("Pool Size entered", poolSize);
+    
+    
+    // ! validation pool size
+    if (!poolSize || poolSize <= 0) {
+        alert("Please enter a valid pool size greater than 0.");
+        return;
+    }
+    console.log("✅ Valid pool size was added", poolSize);
+});
+
 
