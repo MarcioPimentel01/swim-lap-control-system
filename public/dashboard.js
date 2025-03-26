cc20f80625a9ccab0c9dacec2e45e1b06c32bfc7
Marcio Pimentel <149727150+MarcioPimentel01@users.noreply.github.com>
2025-03-26 15:45:28 -0400

    constants added and tested

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,8 +1,19 @@
+console.log("Dashboard JS loaded ✅")
+
 const socket = io(); //! 👈 connects to the server
 
 const poolSizeInput = document.getElementById("pool-size");
+console.log("pool size input", poolSizeInput);
+
 const start = document.querySelector(".start");
+console.log("Start Button", start);
+
 const lapDisplay = document.querySelector(".lap-display");
+console.log("lap Display", lapDisplay);
+
 const lapCounter = document.querySelector(".lap-counter");
+console.log("Lap counter", lapCounter);
+
 const lapTime = document.querySelector(".lap-time");
+console.log("Display lap time", lapTime);
 
