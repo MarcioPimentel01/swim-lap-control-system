1ab4d2471b4f6d15b8a54340be78aa5ba921fa67
Marcio Pimentel <149727150+MarcioPimentel01@users.noreply.github.com>
2025-04-23 17:01:31 -0400

    too tired to continue today, working on version control modal and a function to invoke the version from package.json

--- public/js/utils-html.js
+++ public/js/utils-html.js
@@ -1,3 +1,11 @@
+// tools/inject-version.js
+const { version } = require('../../package.json');
+const fs = require('fs');
+
+fs.writeFileSync('./src/version.js', `export const appVersion = "${version}";\n`);
+
+
+
 //! Only to handle HTML documents logic.
 
 export const initPasswordVisibilityToggle = () => {
@@ -7,6 +15,7 @@ export const initPasswordVisibilityToggle = () => {
     const toggleIconsRegister = document.getElementById("toggle-password-icon-register");
     const passwordInputLogin = document.getElementById("password-input-login");
     const passwordInputRegister = document.getElementById("password-input-register");
+
 
     if (!toggleButtonsLogin || !toggleButtonsRegister || !toggleIconsLogin || !toggleIconsRegister || !passwordInputLogin || !passwordInputRegister) {
     console.warn("Password visibility toggle elements not found.");
@@ -29,3 +38,5 @@ export const initPasswordVisibilityToggle = () => {
         toggleIconsRegister.classList.toggle("bi-eye-slash", !isPasswordRegister);
     });
     };
+
+    //! ✅ Version Modal 
